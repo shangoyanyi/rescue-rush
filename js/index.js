d1b99b4ed76d4addae7ff08a9921600b6979ad3f
chih-kuang,wu <shangoyanyi@gmail.com>
2025-03-14 16:26:49 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,67 @@
+// call 救援電話
+function callRescue(phoneNumber) {
+    window.location.href = `tel:${phoneNumber}`;
+}
+
+// 取得地點並更新 locationInfo 內容
+function updateLocationInfo() {
+    const locationInfo = document.getElementById('location-info');
+
+    if (!navigator.geolocation) {
+        locationInfo.textContent = "瀏覽器不支援查詢位置";
+        return;
+    }
+
+    if(!navigator.onLine){
+        locationInfo.textContent = "目前為離線模式";                
+        return;
+    }
+
+    locationInfo.textContent = "正在取得您的位置...";
+
+    navigator.geolocation.getCurrentPosition(
+        async (position) => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+            const response = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}&zoom=14&addressdetails=1`);
+            const data = await response.json();
+
+            if (data.display_name) {
+                const address = data.address;
+                // 組合完整地址
+                const fullAddress = [
+                    address.country || "",
+                    address.city || address.town || address.village || "",
+                    address.suburb || address.district || "",
+                    address.road || "",
+                    address.house_number || ""
+                ].filter(Boolean).join(", "); // 過濾掉空值，避免多餘的逗號
+
+                locationInfo.textContent = `${fullAddress}`;
+            } else {
+                locationInfo.textContent = `無法取得您的位置，請確保已開啟 GPS 權限`;
+            }
+        },
+        () => {
+            locationInfo.textContent = "無法取得您的位置，請確保已開啟 GPS 權限";
+        }
+    );
+}
+
+
+
+/* ==== js 進入點 ==== */
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("✅ DOM 加載完成，初始化 PWA...");
+
+    // 資料初始化
+    // 取得所在地點資料
+    updateLocationInfo();
+    
+    // 事件監聽
+    // PWA 更新按鈕點擊事件
+    document.getElementById("updateBtn").addEventListener("click", updatePWA);
+
+    // 初始化 firebase (fcm 功能)
+    initFirebase();
+});
